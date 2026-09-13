22ce261a33d165c1a3a4ed283ba385e141421dce
R347H4CK3R <51473529+R347H4CK3R@users.noreply.github.com>
2026-09-13 14:11:15 -0500

    feat: drive Metal first-person camera from Quake player state

--- Sources/ZombiesIOS/Views/QuakeGameplayView.swift
+++ Sources/ZombiesIOS/Views/QuakeGameplayView.swift
@@ -13,7 +13,7 @@ struct QuakeGameplayView: View {
 
     var body: some View {
         ZStack {
-            QuakeMetalWorldView(package: controller.package)
+            QuakeMetalWorldView(package: controller.package, playerState: controller.playerState)
                 .ignoresSafeArea()
 
             HStack(spacing: 0) {
@@ -95,8 +95,9 @@ private struct RuntimeButtonStyle: ButtonStyle {
 
 struct QuakeMetalWorldView: UIViewRepresentable {
     let package: BO2RuntimePackage
+    let playerState: zq3_player_state
 
-    func makeCoordinator() -> Renderer { Renderer(package: package) }
+    func makeCoordinator() -> Renderer { Renderer(package: package, playerState: playerState) }
 
     func makeUIView(context: Context) -> MTKView {
         let view = MTKView(frame: .zero, device: MTLCreateSystemDefaultDevice())
@@ -109,17 +110,29 @@ struct QuakeMetalWorldView: UIViewRepresentable {
         return view
     }
 
-    func updateUIView(_ uiView: MTKView, context: Context) {}
+    func updateUIView(_ uiView: MTKView, context: Context) {
+        context.coordinator.playerState = playerState
+    }
 
     final class Renderer: NSObject, MTKViewDelegate {
+        private struct CameraUniforms {
+            var position: SIMD4<Float>
+            var params: SIMD4<Float> // yaw radians, pitch radians, aspect, vertical FOV tangent
+        }
+
         private var queue: MTLCommandQueue?
         private var pipeline: MTLRenderPipelineState?
+        private var depthState: MTLDepthStencilState?
         private var vertexBuffer: MTLBuffer?
         private var indexBuffer: MTLBuffer?
         private var indexCount = 0
         private let package: BO2RuntimePackage
+        var playerState: zq3_player_state
 
-        init(package: BO2RuntimePackage) { self.package = package }
+        init(package: BO2RuntimePackage, playerState: zq3_player_state) {
+            self.package = package
+            self.playerState = playerState
+        }
 
         func attach(to view: MTKView) {
             guard let device = view.device else { return }
@@ -127,9 +140,24 @@ struct QuakeMetalWorldView: UIViewRepresentable {
             let source = """
             #include <metal_stdlib>
             using namespace metal;
+            struct Camera { float4 position; float4 params; };
             struct VOut { float4 position [[position]]; float3 color; };
-            vertex VOut vmain(const device float3 *p [[buffer(0)]], uint id [[vertex_id]]) {
-                VOut o; float3 q = p[id] * 0.022; o.position=float4(q.x, q.y, q.z, 1.0); o.color=float3(0.55+q.y*0.2,0.68,0.78); return o;
+            vertex VOut vmain(const device float3 *p [[buffer(0)]], constant Camera& camera [[buffer(1)]], uint id [[vertex_id]]) {
+                float3 d = p[id] - camera.position.xyz;
+                float sy = sin(camera.params.x), cy = cos(camera.params.x);
+                float sp = sin(camera.params.y), cp = cos(camera.params.y);
+                float vx = cy * d.x - sy * d.z;
+                float vz0 = sy * d.x + cy * d.z;
+                float vy = cp * d.y - sp * vz0;
+                float vz = sp * d.y + cp * vz0;
+                float aspect = max(camera.params.z, 0.1);
+                float tanHalfFov = max(camera.params.w, 0.1);
+                float nearZ = 0.05;
+                float farZ = 10000.0;
+                VOut o;
+                o.position = float4(vx / (aspect * tanHalfFov), vy / tanHalfFov, (farZ / (farZ-nearZ)) * vz - (farZ*nearZ / (farZ-nearZ)), vz);
+                o.color = float3(0.42 + clamp(p[id].y * 0.004, -0.15, 0.30), 0.66, 0.78);
+                return o;
             }
             fragment float4 fmain(VOut in [[stage_in]]) { return float4(in.color,1.0); }
             """
@@ -141,6 +169,11 @@ struct QuakeMetalWorldView: UIViewRepresentable {
             descriptor.colorAttachments[0].pixelFormat = view.colorPixelFormat
             descriptor.depthAttachmentPixelFormat = view.depthStencilPixelFormat
             pipeline = try? device.makeRenderPipelineState(descriptor: descriptor)
+
+            let depth = MTLDepthStencilDescriptor()
+            depth.isDepthWriteEnabled = true
+            depth.depthCompareFunction = .less
+            depthState = device.makeDepthStencilState(descriptor: depth)
 
             let vertices = package.vertices.map { SIMD3<Float>($0.x, $0.y, $0.z) }
             let indices = package.indices
@@ -161,8 +194,16 @@ struct QuakeMetalWorldView: UIViewRepresentable {
             guard indexCount > 0, let drawable = view.currentDrawable, let pass = view.currentRenderPassDescriptor,
                   let queue, let pipeline, let vertexBuffer, let indexBuffer,
                   let command = queue.makeCommandBuffer(), let encoder = command.makeRenderCommandEncoder(descriptor: pass) else { return }
+            let aspect = Float(max(view.drawableSize.width, 1) / max(view.drawableSize.height, 1))
+            let degreesToRadians = Float.pi / 180
+            var camera = CameraUniforms(
+                position: SIMD4<Float>(playerState.origin.x, playerState.origin.y, playerState.origin.z, 1),
+                params: SIMD4<Float>(playerState.yaw * degreesToRadians, playerState.pitch * degreesToRadians, aspect, tan(70 * degreesToRadians * 0.5))
+            )
             encoder.setRenderPipelineState(pipeline)
+            if let depthState { encoder.setDepthStencilState(depthState) }
             encoder.setVertexBuffer(vertexBuffer, offset: 0, index: 0)
+            encoder.setVertexBytes(&camera, length: MemoryLayout<CameraUniforms>.stride, index: 1)
             encoder.drawIndexedPrimitives(type: .triangle, indexCount: indexCount, indexType: .uint32, indexBuffer: indexBuffer, indexBufferOffset: 0)
             encoder.endEncoding()
             command.present(drawable)
