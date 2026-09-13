57164555ed87cdf746836b586f5ea4631a5e919d
R347H4CK3R <51473529+R347H4CK3R@users.noreply.github.com>
2026-09-13 14:31:38 -0500

    feat: add native BO2 weapon state API

--- Engine/Quake3/zq3_runtime.h
+++ Engine/Quake3/zq3_runtime.h
@@ -9,12 +9,21 @@ extern "C" {
 typedef struct { float x, y, z; } zq3_vec3;
 typedef struct { float forward, right, yaw_delta, pitch_delta; int jump, fire, aim, reload; } zq3_input;
 typedef struct { zq3_vec3 origin; zq3_vec3 velocity; float yaw, pitch; int on_ground; } zq3_player_state;
+typedef struct {
+    int magazine;
+    int reserve;
+    int shots_fired;
+    int reloading;
+    float reload_remaining;
+    float shot_cooldown;
+} zq3_weapon_state;
 
 int zq3_init(void);
 int zq3_load_world(const float *xyz, size_t vertex_count, const uint32_t *indices, size_t index_count, zq3_vec3 spawn);
 void zq3_set_input(zq3_input input);
 void zq3_step(float seconds);
 zq3_player_state zq3_get_player_state(void);
+zq3_weapon_state zq3_get_weapon_state(void);
 void zq3_shutdown(void);
 
 #ifdef __cplusplus
